65bc9783cf57b4d289ffd0e428cc81f5d84e59da
felipemsx <felipe_link64@hotmail.com>
2016-07-26 20:06:05 -0300

    Questão 05 fechada.

--- Questoes/mylib.py
+++ Questoes/mylib.py
@@ -38,12 +38,31 @@ def size(image):
     vector[1] = image.shape[1] #Largura
     return vector
 
+#Questao 05
+def rgb2gray(image):
+    newImage = image.copy()
+    vector = size(newImage)
+    for x in range(0, vector[0]):
+        for y in range(0, vector[1]):
+            greyColor = calcavarage(newImage[x][y][0], newImage[x][y][1], newImage[x][y][2])
+            newImage[x][y][0] = greyColor
+            newImage[x][y][1] = greyColor
+            newImage[x][y][2] = greyColor
+    return newImage
+
+def calcavarage(pixelRed, pixelGreen, pixelBlue):
+    red     = pixelRed * 0.299
+    green   = pixelGreen * 0.587
+    blue    = pixelBlue * 0.114
+    return (red + green + blue) / 3
+
 #testes
 imageRGB = imread('zenfoneGO.jpg')
 imageGrey = imread('imagemCinza.jpg')
-print(nchannels(imageRGB))
-print(nchannels(imageGrey))
-
-print(size(imageRGB))
-print(size(imageGrey))
-print(type(size(imageGrey)[0]))
+# print(nchannels(imageRGB))
+# print(nchannels(imageGrey))
+#
+# print(size(imageRGB))
+# print(size(imageGrey))
+# print(type(size(imageGrey)[0]))
+# rgb2gray(imageRGB)
