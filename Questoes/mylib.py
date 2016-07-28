a85a2b100a1a20faee511487a0ec535e7ab025db
felipemsx <felipe_link64@hotmail.com>
2016-07-28 18:42:07 -0300

    Questão 06 fechada

--- Questoes/mylib.py
+++ Questoes/mylib.py
@@ -4,7 +4,10 @@ import scipy
 
 from PIL import Image
 
-#Questao 02 a)
+MAX = 255
+MIN = 0
+
+#Questao 02 OK!
 def imread(filename):
     image = Image.open(filename)
     return numpy.asarray(image)
@@ -26,27 +29,25 @@ def imread(filename):
 # img = Image.open('test.png')
 # img.resize((50,50)).show()
 
-#Questao 03
+#Questao 03 OK!!!
 # image.layers para imagens
 def nchannels(image):
     return 1 if len(image.shape) == 2 else 3
 
 def isgray(image):
-    return nchannels(image) == 2
+    return nchannels(image) == 1
 
 def isrgb(image):
     return nchannels(image) == 3
 
-
-
-#Questao 04
+#Questao 04 OK!!!
 def size(image):
     vector = range(2)
     vector[0] = image.shape[0] #Altura
     vector[1] = image.shape[1] #Largura
     return vector
 
-#Questao 05
+#Questao 05 OK!!!
 def rgb2gray(image):
     newImage = image.copy()
     vector = size(newImage)
@@ -64,14 +65,39 @@ def calcavarage(pixelRed, pixelGreen, pixelBlue):
     blue    = pixelBlue * 0.114
     return (red + green + blue) / 3
 
-#Questao 06
+#Questao 06 OK!!
 def imreadgray(filename):
     img = imread(filename)
     return rgb2gray(img) if isrgb(img) else img
 
-#testes
+#Questo 07
+#--------------------- Incompelta, nao consigo entender o que o professor quer.
+def imshow(image):
+    Image.fromarray(image).show()
+    return
+
+
+#Questao 08 OK!!
+def thresh(image, limit):
+    newImage = image.copy()
+    vector = size(newImage)
+    if isgray(image):
+        for x in range(0, vector[0]):
+            for y in range(0, vector[1]):
+                newImage[x][y] = MAX if (newImage[x][y] >= limit) else MIN
+    else:
+        for x in range(0, vector[0]):
+            for y in range(0, vector[1]):
+                newImage[x][y][0] = MAX if (newImage[x][y][0] >= limit) else MIN
+                newImage[x][y][1] = MAX if (newImage[x][y][0] >= limit) else MIN
+                newImage[x][y][2] = MAX if (newImage[x][y][0] >= limit) else MIN
+    return newImage
+
+#testes----------------------------------------------------
 imageRGB = imread('zenfoneGO.jpg')
-imageGrey = imread('imagemCinza.jpg')
+imageGrey = imread('zenfoneGOGrey.jpg')
+energiaRG = imread('Energia.jpg')
+energiaGrey = imread('EnergiaCinza.jpg')
 # print(nchannels(imageRGB))
 # print(nchannels(imageGrey))
 #
@@ -80,5 +106,5 @@ imageGrey = imread('imagemCinza.jpg')
 # print(type(size(imageGrey)[0]))
 # rgb2gray(imageRGB)
 
-Image.fromarray(imreadgray('zenfoneGO.jpg')).show()
-Image.fromarray(imreadgray('imagemCinza.jpg')).show()
+#Questao 08
+#Image.fromarray(thresh(imageGrey,170)).show()
